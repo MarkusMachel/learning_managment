7e9e649239a86fd0fe3f48191f73e24797dac369
MarkusMachel <machelmarkus@gmail.com>
2024-07-25 19:29:22 -0300

    Course preview

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,7 @@
 class Lesson < ApplicationRecord
-  has_one_attached :video
+  has_one_attached :video do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
 
   belongs_to :course
 end
